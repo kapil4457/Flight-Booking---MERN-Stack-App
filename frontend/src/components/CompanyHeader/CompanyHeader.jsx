679142f85920bc92ed6d411cb5e595fd8d2c28f6
Kapil Soni <kapilsoni54768161@gmail.com>
2023-03-08 15:14:12 +0530

    Final Submission

--- frontend/src/components/CompanyHeader/CompanyHeader.jsx
+++ frontend/src/components/CompanyHeader/CompanyHeader.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./CompanyHeader.scss";
 import logo from "./logo.png";
+import { useNavigate } from "react-router-dom";
 const CompanyHeader = () => {
+  const navigate = useNavigate();
   return (
-    <div className="company-heading color-change">
+    <div
+      className="company-heading color-change"
+      onClick={() => {
+        navigate("/");
+      }}
+    >
       <img src={logo} alt="" />
       JMR Airlines
     </div>
